14ecca33bfb9c83520e57233099b7c1307d26635
chenbaoquan <569532805@qq.com>
2017-05-25 14:02:15 +0800

    wrote 3 files

---
+++ 用parseInt解析数字，并求和/demo.js
@@ -0,0 +1,14 @@
+﻿window.onload=function(){
+	var oInput = document.getElementsByTagName("input");
+	var oSpan = document.getElementsByTagName("span")[0];
+	for(var i=0;i<oInput.length-1;i++){
+		oInput[i].onkeyup = function(){
+			this.value = this.value.replace(/[^\d]/,"")
+		}
+	}
+
+	oInput[2].onclick = function(){
+		(oInput[0].value == "" || oInput[1].value == "") ? alert("请输入数字") : oSpan.innerHTML = parseInt(oInput[0].value) + parseInt(oInput[1].value);
+
+	}
+};
